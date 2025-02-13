17116c57c2325eeeae290a12a26ab17a5798d19f
aji <mancode700@gmail.com>
2025-02-13 12:51:33 +0700

    UPDATE: .env dan config/app.php

--- config/app.php
+++ config/app.php
@@ -51,9 +51,9 @@ return [
     | the application so that it's available within Artisan commands.
     |
     */
-    'url' => env('APP_URL', 'https://website-hrd-production.up.railway.app/'),
+    'url' => env('APP_URL', 'https://sim-hrd-pt-ppi.up.railway.app/'),
 
-    'asset_url' => env('ASSET_URL', 'https://website-hrd-production.up.railway.app/'),
+    'asset_url' => env('ASSET_URL', 'https://sim-hrd-pt-ppi.up.railway.app/'),
 
     /*
     |--------------------------------------------------------------------------
